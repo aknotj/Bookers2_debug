1f0caecc8350014d256ba1cd140974237a8d9b70
aknotj <100099666+aknotj@users.noreply.github.com>
2022-04-06 09:49:48 +0000

    [Finish] test 1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,29 @@
 class BooksController < ApplicationController
 
-  def show
-    @book = Book.find(params[:id])
-  end
-
-  def index
-    @books = Book.all
+  def new
+    @book = Book.new(book_params)
   end
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book.id), notice: "You have created book successfully."
     else
       @books = Book.all
-      render 'index'
+      render :index
     end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @newbook = Book.new
+    @user = @book.user
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def edit
@@ -27,19 +35,19 @@ class BooksController < ApplicationController
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
-      render "edit"
+      render :edit
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
