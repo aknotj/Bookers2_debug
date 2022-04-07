59ede419ae33dd42f4be0bbaab578d8099b6d4aa
aknotj <100099666+aknotj@users.noreply.github.com>
2022-04-07 01:13:37 +0000

    [Add] relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,18 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  
+  has_many :received_follows, foreign_key: :followed_id, class_name: "Relationship", dependent: :destroy
+  # Will return an array of users who follow the user instance
+  has_many :followers, through: :received_follows, source: :follower
+
+  # returns an array of follows a user gave to someone else
+  has_many :given_follows, foreign_key: :follower_id, class_name: "Follow"
+  
+  # returns an array of other users who the user has followed
+  has_many :followings, through: :given_follows, source: :followed_user
+  
+  
   has_one_attached :profile_image
 
   validates :name, presence: true, length: { minimum: 2, maximum: 20 }, uniqueness: true
